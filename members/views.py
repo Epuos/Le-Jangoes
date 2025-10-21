d7a1e89ac10fab53b71f161b7aba034614a557c8
Jason Zhong <jazh2@Jerry-Wans-Windows-XP-Gaming-Laptop-with-Red-Star-OS.local>
2025-10-22 10:10:24 +1100

    It doesnt work on workspace

---
+++ members/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+def members(request):
+  template = loader.get_template('html.html')
+  return HttpResponse(template.render())
